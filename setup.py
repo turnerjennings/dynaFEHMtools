4a1612fa7a016e370261ba9e11a64f1dc32b168d
trnrjngs <145458889+trnrjngs@users.noreply.github.com>
2023-12-11 17:19:04 -0500

    create license

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dynaFEHMtools",
+    version='0.1dev',
+    author='Turner Jennings',
+    author_email='turner.jennings@outlook.com',
+    packages=['dynafehmtools'],
+    license='LICENSE.txt',
+    description='Tools for FE head model data condensation in LS-DYNA'
+    )
